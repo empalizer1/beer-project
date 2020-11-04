242fecf2883feea917e97bd57769c94175ba5190
empalizer1 <71009323+empalizer1@users.noreply.github.com>
2020-11-04 22:30:51 +0100

    something to start with

--- search.js
+++ search.js
@@ -1,1 +1,38 @@
-/* Javascript for Search Page */
+const apiBaseUrl = "https://api.punkapi.com/v2/beers";
+let page = 1;
+let searchValue;
+const searchButton = document.querySelector('#searchButton');
+const searchInput = document.querySelector('#searchInput');
+const nextButton = document.querySelector('.next');
+const prevButton = document.querySelector('.prev');
+
+const fetchBeer = () => {
+    fetch(`${apiBaseUrl}?page=${page}&per_page=10&beer_name=${searchValue}`)
+        .then(response => response.json())
+        .then(data => {
+            /* CREATE HTML LIST */
+            console.log(data);
+            for(let beer of data){
+                console.log(beer.name);
+            }
+        })
+}
+
+searchButton.addEventListener('click', (target) => {
+    target.preventDefault();
+    searchValue = searchInput.value;
+    page = 1;
+    fetchBeer();
+})
+
+nextButton.addEventListener('click',() => {
+        page++;
+        fetchBeer();
+})
+
+prevButton.addEventListener('click',() => {
+    if(page > 1) {
+    page--;
+    fetchBeer();
+    }
+})
